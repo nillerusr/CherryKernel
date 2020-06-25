3f511d6deb1c0f826133c48102ac37e0bbbb2b2b
nillerusr <42746659+nillerusr@users.noreply.github.com>
2020-06-25 04:49:14 +0300

    Fix build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,27 @@
 #!/bin/sh
 
-export CROSS_COMPILE=$(pwd)gcc/bin/arm-linux-gnueabihf-
+export CROSS_COMPILE=$(pwd)/gcc/bin/arm-linux-gnueabihf-
 export USE_CCACHE=1
 export ARCH=arm
 export TARGET=out
 
 configure()
 {
+  echo Configuring
   make O=$TARGET ARCH=$ARCH cactus_defconfig
 }
 
 build()
 {
+  echo Building
   make O=$TARGET ARCH=$ARCH -j$(nproc --all)
 }
 
 if ! [ -d gcc ]
 then
+  echo Downloading Linaro GCC
   wget -q https://releases.linaro.org/components/toolchain/binaries/7.5-2019.12/arm-linux-gnueabihf/gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
+  echo Unpacking Linaro GCC
   tar -xJf gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
   mv gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf gcc
   rm gcc-linaro-7.5.0-2019.12-x86_64_arm-linux-gnueabihf.tar.xz
